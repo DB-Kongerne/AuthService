401969dd0b2b7eb1bc46f9ba78f4bba8fa776365
Cosmo99one <ruaengladaphirom@gmail.com>
2024-11-05 13:08:45 +0100

    godtt

--- Program.cs
+++ Program.cs
@@ -1,6 +1,42 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.Commons;
+
+var EndPoint = "https://localhost:8201/";
+var httpClientHandler = new HttpClientHandler();
+httpClientHandler.ServerCertificateCustomValidationCallback =
+(message, cert, chain, sslPolicyErrors) => { return true; };
+
+// Initialize one of the several auth methods.
+IAuthMethodInfo authMethod =
+new TokenAuthMethodInfo("00000000-0000-0000-0000-000000000000");
+// Initialize settings. You can also set proxies, custom delegates etc. here.
+var vaultClientSettings = new VaultClientSettings(EndPoint, authMethod)
+{
+    Namespace = "",
+    MyHttpClientProviderFunc = handler
+    => new HttpClient(httpClientHandler)
+    {
+        BaseAddress = new Uri(EndPoint)
+    }
+};
+IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+
+// Use client to read a key-value secret.
+Secret<SecretData> kv2Secret = await vaultClient.V1.Secrets.KeyValue.V2
+.ReadSecretAsync(path: "hemmeligheder", mountPoint: "secret");
+var issuer = kv2Secret.Data.Data["Issuer"];
+Console.WriteLine($"Issuer: {issuer}");
+
+// Use client to read a key-value secret.
+Secret<SecretData> kv3Secret = await vaultClient.V1.Secrets.KeyValue.V2
+.ReadSecretAsync(path: "hemmeligheder", mountPoint: "secret");
+var secret = kv2Secret.Data.Data["Secret"];
+Console.WriteLine($"Secret: {secret}");
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,17 +46,17 @@ builder.Services
 .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 .AddJwtBearer(options =>
 {
-options.TokenValidationParameters = new TokenValidationParameters()
-{
-ValidateIssuer = true,
-ValidateAudience = true,
-ValidateLifetime = true,
-ValidateIssuerSigningKey = true,
-ValidIssuer = myIssuer,
-ValidAudience = "http://localhost",
-IssuerSigningKey =
-new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mySecret))
-};
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = myIssuer,
+        ValidAudience = "http://localhost",
+        IssuerSigningKey =
+    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mySecret))
+    };
 });
 
 // Add services to the container.
