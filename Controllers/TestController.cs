858b2a239abb0b1a557a68f8a6eb75db8c3978e6
Cosmo99one <ruaengladaphirom@gmail.com>
2024-11-05 11:01:54 +0100

    Dyt

---
+++ Controllers/TestController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("You're authorized"); //Husk at export Secret og Issuer (Test med echoo $)//
+        }
+    }
+}
